7076225ffb0cec2a2fec7464685647c0078ef9b7
Mokayed <marik.3000@hotmail.com>
2019-05-06 10:48:55 +0200

    login

--- src/main/java/Entities/Subject.java
+++ src/main/java/Entities/Subject.java
@@ -14,22 +14,5 @@ import java.util.List;
  */
 public class Subject {
 
-    public static void main(String[] args) {
-        List<User> list = new ArrayList<>();
-        list.add(new User("        List<User> list = new ArrayList<>();\n" +
-"        String p = null;Student", "Lasse", "123", 23, true));
-        list.add(new User("Teacher", "Kasper", "321", "20-05-2010", "experiance in danish langouge"));
-        for (User user : list) {
-            if (user.getUserName().equals("Kasper") && user.getPassword().equals("321")) {
-                System.out.println("her...");
-                if (user.getRole().equals("Student")) {
-                    System.out.println(user.getUserName());
-                }
-                if (user.getRole().equals("Teacher")) {
-                    System.out.println(user.getUserName());
-                }
-            }
-        }
-
-    }
+ 
 }
